7f9bc667165747434f08b6004fc8c89c1e8148eb
Siddharth189 <79472473+Siddharth189@users.noreply.github.com>
2021-03-27 22:44:46 +0530

    Update 2.c

--- Assignment-12/2.c
+++ Assignment-12/2.c
@@ -15,40 +15,105 @@ struct node
     struct node* next;
 };
 
-void addAtbegin(struct node** temp, int data)
+struct node* newNode(int data)
+{
+    struct node* p = (struct node*) malloc(sizeof(struct node));
+
+    p -> data = data;
+    p -> next = NULL;
+
+    return (p);
+}
+
+struct node* addAtbegin(struct node* temp, int data)
 {
     struct node* t = (struct node*)malloc(sizeof(struct node));
     t->data = data;
-    t->next = *temp;
-    *temp = t;
+    t->next = temp;
+    temp = t;
 
+    return (temp);
 }
-void addAtLast(struct node** temp, int data)
+struct node* addAtLast(struct node* temp, int data)
 {
-    struct node* t = (struct node*)malloc(sizeof(struct node));
-    while((*temp)->next)
+    struct node* t = (struct node*) malloc(sizeof(struct node));
+    struct node* tempa = temp;
+    while((temp -> next) != NULL)
     {
-
+        temp = temp->next;
     }
+
     t->data = data;
     t->next = NULL;
-    (*temp)->next = t;
+    (temp)->next = t;
 
+    return tempa;
 }
-void addAtPosition(struct node** temp, int data, int i)
+struct node* addAtPosition(struct node* temp, int data, int i)
 {
     struct node* t = (struct node*)malloc(sizeof(struct node));
 
+    struct node* tempa = temp;
 
-    while(i - 1  && *temp)
+    if(i == 0)
+        tempa = addAtbegin(temp, data);
+    else
     {
-        (*temp) = (*temp)->next;
-        i--;
-    }
-    t->data = data;
-    t->next = (*temp)->next;
-    (*temp)->next = t;
+        while((i - 1) && (temp->next))
+        {
+            (temp) = (temp)->next;
+            i--;
+        }
+        t->data = data;
+        t->next = (temp)->next;
+        (temp)->next = t;
 
+    }
+    return tempa;
+}
+
+int deleteAtbegin(struct node** temp)
+{
+    int data = (*temp)->data;
+    (*temp) = (*temp)->next;
+
+    return (data);
+}
+
+int deleteAtLast(struct node** temp)
+{
+    struct node* flag = *temp;
+
+    while(((*temp)->next->next))
+    {
+        (*temp) = ((*temp)->next);
+    }
+    int data = ((*temp)->next->data);
+    (*temp)->next = NULL;
+
+    *temp = flag;
+    return data;
+}
+int deleteAtPosition(struct node** temp, int i)
+{
+    int data = 0;
+    if(i == 0)
+        deleteAtbegin(temp);
+    else
+    {
+        struct node* flag = *temp;
+        while((i - 1) && ((*temp)->next->next))
+        {
+            (*temp) = (*temp)->next;
+            i--;
+        }
+
+        data = (*temp)->next->data;
+        (*temp)->next = NULL;
+
+        *temp = flag;
+        return (data);
+    }
 }
 
 void Display(struct node* p)
@@ -62,17 +127,40 @@ void Display(struct node* p)
 }
 int main()
 {
-    struct node* p = NULL;
+    struct node* head = NULL;
+    head = newNode(5);
 
-    addAtbegin(&p, 8);
-    Display(p);
-    addAtLast(&p, 16);
-    Display(p);
-    addAtPosition(&p, 10, 1);
-    Display(p);
-    addAtPosition(&p, 34, 1);
-    Display(p);
+    Display(head);
+
+    head = addAtbegin(head, 10);
+
+    Display(head);
+
+    head = addAtLast(head, 15);
+
+    Display(head);
+
+    head = addAtPosition(head, 1000, 1);
+
+    Display(head);
+
+    int dataATBegin = deleteAtbegin(&head);
+
+    Display(head);
+    printf("Deleted data is %d\n\n", dataATBegin);
+
+    int dataATLast = deleteAtLast(&head);
+
+    Display(head);
+    printf("Deleted data is %d\n\n", dataATLast);
+
+
+    int dataATPosition = deleteAtPosition(&head, 0);
+
+    Display(head);
+    printf("Deleted data is %d\n", dataATPosition);
+
+
 
     return 0;
 }
-
